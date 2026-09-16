729862905a1e4e33ba7d43b98409a0d16f3cbe57
pranjalijahagirdar <pranjalijahagirdar15@gmail.com>
2026-09-16 20:08:45 +0530

    add create functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,13 @@
 const cl=console.log;
 
+const postContainer = document.getElementById('postContainer')
+const titleControl = document.getElementById('titleControl')
+const bodyControl = document.getElementById('bodyControl')
+const userId = document.getElementById('userId')
+const postForm = document.getElementById('postForm')
+const addCardbtn = document.getElementById('addCardbtn')
+const updateCardbtn = document.getElementById('updateCardbtn')
+
 const BASE_URL = `https://jsonplaceholder.typicode.com/`
 const POST_URL = `${BASE_URL}/posts`
 
@@ -34,3 +42,83 @@ xhr.onload = function(){
         })
     }
 }
+
+
+//create
+
+function onAddcard(eve){
+    eve.preventDefault()
+    let postObj={
+        titleControl : titleControl.value,
+        bodyControl : bodyControl.value,
+        userId : userId.value,
+    }
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", POST_URL)
+    xhr.send(JSON.stringify(postObj));
+    
+    
+    xhr.onload = function (){
+        if(xhr.status === 201){
+            let res = JSON.parse(xhr.response)
+            let col = document.createElement('div');
+            col.className = 'col-md-4 mb-4'
+            col.id = res.id;
+            col.innerHTML = `<div class="card h-100">
+                    <div class="card-header">
+                        <h3 class="m-0">${postObj.titleControl}</h3>
+                    </div>
+                        <div class="card-body">
+                            <p class="m-0">${postObj.bodyControl}</p>
+                        </div>
+                        <div class="card-footer d-flex justify-content-between">
+                            <button onclick="editCard(this)" class="btn btn-sm btn-primary">Edit</button>
+                            <button onclick="removeCard(this)" class="btn btn-sm btn-danger">Remove</button>
+                        </div>
+                </div>`
+            
+            const postContainer = document.getElementById('postContainer')
+            postContainer.prepend(col)
+            postForm.reset()
+        }else{
+            cl(`something went wrong !!!`)
+        }
+    }
+
+    Swal .fire({
+        title:"card created successfully !!!",
+        icon:"success",
+        timer:3000
+    })
+}
+
+//edit 
+
+// function onEdit(ele){
+//     let EDIT_ID = ele.closest('.col-md-4').id;
+//     localStorage.setItem("EDIT_ID", EDIT_ID);
+//     let SINGLE_POST_URL = `${BASE_URL}/posts/${EDIT_ID}`
+//     let xhr = new XMLHttpRequest();
+//     xhr.open("GET", SINGLE_POST-URL);
+//     xhr.send(null);
+
+//     xhr.onload = function (){
+//         if(xhr.status === 200){
+//             let res = JSON.parse(xhr.response);
+//             cl(res);
+//             //patch data in form-controls
+//             titleControl.value = res.titleControl;
+//             bodyControl.value = res.bodyControl;
+//             userId.value = res.userId;
+//             addCardbtn.classList.add('d-none')
+//             updateCardbtn.classList.remove('d-none')
+
+//         }else{
+//             cl(`something went wrong !!!`)
+//         }
+//     }
+// }
+
+
+
+postForm.addEventListener('submit', onAddcard)
